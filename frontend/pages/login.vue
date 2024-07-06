f56e6c65ceb8d578e8d11524e163d38958c3cc8f
Mario Susanto <msus0004@student.monash.edu>
2024-07-06 20:22:12 +0700

    created login page (without functionality)

---
+++ frontend/pages/login.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="flex flex-row min-h-screen">
+        <div class="bg-custom-bg-green grow flex items-center justify-center">
+            <img src="../assets/img/HeartyMealLoginLogo.png" alt="Hearty Meal">
+        </div>
+        <div class="grow flex items-center justify-center">
+            <div class="space-y-5">
+                <H1 class="">Login to your account</H1>
+                <H2>Welcome back 👋</H2>
+                <Overlay :level="1" class="flex flex-col space-y-5">
+                    <div class="space-y-0">
+                        <P><b>Email</b></P>
+                        <Input placeholder="email"></Input>
+                    </div>
+                    <Button class="bg-custom-button-orange hover:bg-custom-button-orange text-custom-text-orange w-full">Verify</Button>
+                </Overlay>
+                <div class="flex flex-row space-x-2 items-center">
+                    <P>Don't have an account?</P>
+                    <Button class="bg-transparent hover:bg-transparent shadow-none text-custom-text-orange">Sign up</Button>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+</template>
+<script setup>
+defineOptions({
+	name: "HomePage",
+});
+
+
+definePageMeta({
+	layout: "emptylayout"
+});
+</script>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -17,6 +17,7 @@ export default {
         'custom-overlay-light': '#FFFEF1',
         'custom-button-orange': '#FFA17A',
         'custom-button-green': '#87A98D',
+        'custom-text-orange': '#993300',
       },
     },
   },
