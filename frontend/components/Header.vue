0984bce89ce8f0839444d470cb749c888da3d625
Mario Susanto <msus0004@student.monash.edu>
2024-07-14 19:16:53 +0700

    added logo to header

--- frontend/components/Header.vue
+++ frontend/components/Header.vue
@@ -1,14 +1,8 @@
 <template>
 	<div class="relative bg-custom-bg-green shadow-md">
 		<div class="mx-auto container px-5">
-			<div class="flex items-center justify-between py-3 ">
-                <h1 class="text-2xl text-white">Hearty Meals</h1>
-                <div class="flex space-x-5">
-                    <button class="rounded-md bg-custom-button-orange text-white py-2 px-5">1</button>
-                    <button class="rounded-md bg-custom-button-orange text-white py-2 px-5">2</button>
-                    <button class="rounded-md bg-custom-button-orange text-white py-2 px-5">3</button>
-                    <button class="rounded-md bg-custom-button-orange text-white py-2 px-5">4</button>
-                </div>
+			<div class="flex items-center justify-center py-3 ">
+                <img src="../assets/img/HeartyMealHeaderLogo.png" alt="Hearty Meal" :width="200">
             </div>
 		</div>
 	</div>
