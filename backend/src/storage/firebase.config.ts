489ba28c9ba27c62d58bdc21755e8a79a18517ee
jchu0057 <jchu0057@student.monash.edu>
2024-07-29 16:44:08 +0800

    Reconfigured firebase configuration settings

--- backend/src/storage/firebase.config.ts
+++ backend/src/storage/firebase.config.ts
@@ -10,27 +10,27 @@ function initialiseFirebase(){
 
   // Your web app's Firebase configuration
   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  // const firebaseConfig = {
-  //   apiKey: process.env.FIREBASE_API_KEY,
-  //   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  //   projectId: process.env.FIREBASE_PROJECT_ID,
-  //   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  //   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  //   appId: process.env.FIREBASE_APP_ID,
-  //   measurementId: process.env.FIREBASE_MEASUREMENT_ID
-  // };
-
-  // // Initialize Firebase
-  // const app = initializeApp(firebaseConfig);
-
-  // const storage = getStorage();
 
   var admin = require("firebase-admin");
 
-  var serviceAccount = require("../../hf-nutrition-firebase-adminsdk-n9w73-b1e7fe6f15.json");
+  const firebase_config = {
+    "type": process.env.FIREBASE_TYPE,
+    "project_id": process.env.FIREBASE_PROJECT_ID,
+    "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
+    "private_key": process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    "client_email": process.env.FIREBASE_CLIENT_EMAIL,
+    "client_id": process.env.FIREBASE_CLIENT_ID,
+    "auth_uri": process.env.FIREBASE_AUTH_URI,
+    "token_uri": process.env.FIREBASE_TOKEN_URI,
+    "auth_provider_x509_cert_url": process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+    "client_x509_cert_url": process.env.FIREBASE_CLIENT_X509_CERT_URL,
+    "universe_domain": process.env.FIREBASE_UNIVERSE_DOMAIN
+  };
+
+  // var serviceAccount = require(JSON.stringify(firebase_config));
 
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
+    credential: admin.credential.cert(firebase_config),
     storageBucket: process.env.FIREBASE_STORAGE_BUCKET
   });
 }
