9226b8f0121a1227578fbdd2f5d3fa598c27e68b
Mhar0027 <mhar0027@student.monash.edu>
2024-08-04 16:55:41 +0800

    Commit initial work for Part 4 of the Landing Page

---
+++ frontend/components/RecipeOfTheDay.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="relative m-20 w-[70vw] h-[60vh] flex items-center justify-center overflow-hidden">
+      <img src="@/assets/img/dailyMealBG.png" alt="Hearty Meal" class="absolute w-full h-full z-0">
+      <div class="absolute z-10 flex flex-col items-center text-white p-4">
+        <h2 class="text-2xl font-bold">Recipe of the Day</h2>
+        <div class="flex space-x-16 mt-2 w-full px-16">
+          <div class="w-1/2">
+            <p class="text-justify">
+              {{ recipeText }}
+            </p>
+            <div>
+                <img src="@/assets/img/IconGetRecipe.png" alt="Hearty Meal" class=" w-fit h-fit z-0">
+                <p>Get Recipe</p>
+            </div>
+          </div>
+          <div class="w-1/2 flex flex-col items-center justify-center">
+            <img src="@/assets/img/nasiAyam.png" alt="Hearty Meal" class=" w-fit h-fit z-0">
+            <H2 class="text-justify">{{ recipeName }}</H2>
+          </div>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script setup>
+  const props = defineProps({
+    recipeName: {
+        type: String,
+        default: "Nasi Ayam"
+    },
+    recipeText: {
+        type: String,
+        default: "Lorem ipsum dolor sit amet, amogus sus baguette hon hon eiffel tower. This is not a test, this is a simulation theory to test what is and what is not, for what is stands against what is not."
+    }
+  })
+  </script>
+  
+  <style scoped>
+  /* Tailwind CSS handles most styling; no additional CSS is required */
+  </style>
+  
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,9 +1,6 @@
 <template>
-    <div class="flex items-center justify-center h-96">
-        <div class="shadow-sm bg-custom-overlay-brown h-40 w-64 rounded-lg flex flex-col items-center justify-center space-y-2">
-            <p class="font-medium text-md">Nuxt JS Guide</p>
-            <button class="rounded-sm shadow-sm bg-custom-button-green py-1 px-2 text-white hover:bg-custom-button-green" @click.prevent="onClickButton">Button</button>
-        </div>
+    <div class="flex items-center justify-center">
+        <RecipeOfTheDay recipeName="burger"/>
     </div>
 </template>
 <script setup>
