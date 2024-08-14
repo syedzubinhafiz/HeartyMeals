38551bdf79e91c195de6f0024b73c26cca4ce7ff
Mario Susanto <msus0004@student.monash.edu>
2024-08-14 17:28:35 +0800

    replaced screenshotted green curve with figma exported version

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -4,7 +4,7 @@
             <img src="../assets/img/HeartyMealLargeLogo.png" alt="Hearty Meal">
         </div>
         <div class="w-3/5 flex items-center">
-            <img src="../assets/img/GreenCurve.png" class="h-full" alt="Green Curve Decor">
+            <img src="../assets/img/GreenCurve.png" class="h-screen" alt="Green Curve Decor">
             <img src="../assets/img/BrownBlob.png" class="w-80 h-80 absolute bottom-0 right-0" style="object-fit: cover; object-position: 220px 120px;" alt="Brown Blob Decor">
             <div class="w-full flex justify-center">
                 <div class="space-y-5 m-9 z-10">
--- frontend/pages/signup.vue
+++ frontend/pages/signup.vue
@@ -4,7 +4,7 @@
             <img src="../assets/img/HeartyMealLargeLogo.png" alt="Hearty Meal">
         </div>
         <div class="w-3/5 flex items-center justify-center">
-            <img src="../assets/img/GreenCurve.png" class="h-full" alt="Green Curve Decor">
+            <img src="../assets/img/GreenCurve.png" class="h-screen" alt="Green Curve Decor">
             <img src="../assets/img/BrownBlob.png" class="w-80 h-80 absolute bottom-0 right-0" style="object-fit: cover; object-position: 220px 120px;" alt="Brown Blob Decor">
             <div class="space-y-5 m-9 sm:mx-24 grow z-10">
                 <H1>Create an account</H1>
