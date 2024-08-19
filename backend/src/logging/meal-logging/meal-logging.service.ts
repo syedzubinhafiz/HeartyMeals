32a944a7a3970f95d55971772d6aadcb77d002ca
jchu0057 <jchu0057@student.monash.edu>
2024-08-19 16:26:14 +0800

    Added module file and created basic service file

---
+++ backend/src/logging/meal-logging/meal-logging.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { MealLogging } from "./meal-logging.entity";
+import { Repository } from "typeorm";
+
+@Injectable()
+export class MealLoggingService {
+    constructor(
+        @InjectRepository(MealLogging)
+        private mealLoggingRepository: Repository<MealLogging>,
+    ){}
+}
