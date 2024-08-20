46db350f523281ad8f089d7e6e77d83a22a522b3
jchu0057 <jchu0057@student.monash.edu>
2024-08-20 12:44:58 +0800

    Updated the markIsConsumed endpoint return lines

--- backend/src/logging/meal-logging/meal-logging.controller.ts
+++ backend/src/logging/meal-logging/meal-logging.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import { Body, Controller, HttpException, Post } from "@nestjs/common";
 import { MealLoggingService } from "./meal-logging.service";
 
 @Controller('meal-logging')
@@ -9,6 +9,13 @@ export class MealLoggingController {
 
     @Post('mark_consumed')
     async markMealConsumed(@Body("mealLoggingId") payload){
-        return await this.mealLoggingService.markIsConsumed(payload.mealLoggingId);
+        try {
+            await this.mealLoggingService.markIsConsumed(payload.mealLoggingId);
+        }
+        catch (e){
+            return new HttpException(e.message, 400);
+        }
+
+        return new HttpException("Meal is marked consumed.", 200);
     }
 }
