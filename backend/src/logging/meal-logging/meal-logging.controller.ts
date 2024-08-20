cd126324f261539f70f37667c832aed68b432011
jchu0057 <jchu0057@student.monash.edu>
2024-08-20 12:20:47 +0800

    Added mark meal as consumed method and endpoint

--- backend/src/logging/meal-logging/meal-logging.controller.ts
+++ backend/src/logging/meal-logging/meal-logging.controller.ts
@@ -1,6 +1,14 @@
-import { Controller } from "@nestjs/common";
+import { Body, Controller, Post } from "@nestjs/common";
+import { MealLoggingService } from "./meal-logging.service";
 
 @Controller('meal-logging')
 export class MealLoggingController {
+    constructor(
+        private mealLoggingService: MealLoggingService,
+    ){}
 
+    @Post('mark_consumed')
+    async markMealConsumed(@Body("mealLoggingId") payload){
+        return await this.mealLoggingService.markIsConsumed(payload.mealLoggingId);
+    }
 }
--- backend/src/logging/meal-logging/meal-logging.service.ts
+++ backend/src/logging/meal-logging/meal-logging.service.ts
@@ -10,7 +10,27 @@ export class MealLoggingService {
         private mealLoggingRepository: Repository<MealLogging>,
     ){}
 
-    async addMealLogging(){
+    async addMealLogging(userId, recipeId, mealType){
         
+    }
+
+    async getMealLogging(){}
+
+    /**
+     * Mark the meal consumed 
+     * @param mealLoggingId - unique id of the meal
+     * @returns true after the entry is saved to the database
+     */
+    async markIsConsumed(mealLoggingId){
+        try {
+            var entry = await this.mealLoggingRepository.findOneBy({id: mealLoggingId});
+        }
+        catch (e){
+            return e;
+        }
+
+        entry.is_consumed = true;
+        await this.mealLoggingRepository.save(entry);
+        return true;
     }
 }
