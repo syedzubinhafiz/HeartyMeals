bc565f77928375954fad542427dc932219d27371
jchu0057 <jchu0057@student.monash.edu>
2024-08-20 14:18:20 +0800

    Disabled firebase initialisation and updated debug value in env copy

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -63,6 +63,9 @@ export class AppModule {
     seedEthnicity(this.dataSource);
     seedFoodCategory(this.dataSource);
     seedDietary(this.dataSource);
-    initialiseFirebase();
+    if (process.env.DEBUG === "true"){
+      initialiseFirebase();
+    }
+    
   }
 }
