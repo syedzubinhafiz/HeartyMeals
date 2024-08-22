37952c7556d02b1d760b01e09347450e73cc2c5c
gsoo0005 <gsoo0005@student.monash.edu>
2024-08-23 03:12:29 +0800

    Fix minor error and tested the logic with no user allergy

--- backend/src/component/component.controller.ts
+++ backend/src/component/component.controller.ts
@@ -1,16 +1,39 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Headers, Post } from '@nestjs/common';
 import { ComponentService } from './component.service';
 import { AddComponentDTO } from './dto/add-component-dto';
+import { CommonService } from 'src/common/common.service';
+import { ComponentType } from './enum/type.enum';
 
 @Controller('component')
 export class ComponentController {
 
     constructor(
-        private componentService: ComponentService
+        private componentService: ComponentService,
+        private commonService: CommonService
     ){}
 
     @Post()
     async add(@Body() payload: AddComponentDTO){
         return this.componentService.add(payload);
     }
+    
+    @Post('add/bulk')
+    async addBulk(@Body() payload: AddComponentDTO[]){
+        return this.componentService.addBulk(payload);
+    }
+
+    @Get("ingredients")
+    async getIngredients(@Headers() headers: any){
+        const authHeader = headers.authorization;
+        const decodedHeader = this.commonService.decodeHeaders(authHeader);
+        return this.componentService.getComponents(decodedHeader, ComponentType.INGREDIENT);
+    }
+
+    @Get("seasonings")
+    async getSeasonings(@Headers() headers: any){
+        const authHeader = headers.authorization;
+        const decodedHeader = this.commonService.decodeHeaders(authHeader);
+        return this.componentService.getComponents(decodedHeader, ComponentType.SEASONING);
+    }
+
 }
--- backend/src/component/component.service.ts
+++ backend/src/component/component.service.ts
@@ -4,6 +4,8 @@ import { Component } from './component.entity';
 import { In, Repository } from 'typeorm';
 import { AddComponentDTO } from './dto/add-component-dto';
 import { FoodCategory } from 'src/food-category/foodCategory.entity';
+import { UserAllergy } from 'src/allergy/user_allergy.entity';
+import { ComponentType } from './enum/type.enum';
 
 @Injectable()
 export class ComponentService {
@@ -12,7 +14,10 @@ export class ComponentService {
         @InjectRepository(Component)
         private componentRepository: Repository<Component>,
         @InjectRepository(FoodCategory)
-        private foodCategoryRepository: Repository<FoodCategory>
+        private foodCategoryRepository: Repository<FoodCategory>,
+        @InjectRepository(UserAllergy)
+        private userAllergyRepository: Repository<UserAllergy>
+
     ){}   
 
     async add(payload: AddComponentDTO) {
@@ -43,4 +48,73 @@ export class ComponentService {
 
         return new HttpException("Component added successfully", 200);
     }
+
+    async addBulk(payloads: AddComponentDTO[]) {
+
+        // Get all unique foodCategoryIds from the payloads
+        const foodCategoryIds = [...new Set(payloads.map(payload => payload.foodCategoryId))];
+    
+        // Fetch all food categories in a single query
+        const foodCategories = await this.foodCategoryRepository.findByIds(foodCategoryIds);
+    
+        // Create a map for easy access to categories by id
+        const foodCategoryMap = new Map<string, FoodCategory>();
+        foodCategories.forEach(category => {
+            foodCategoryMap.set(category.id, category);
+        });
+    
+        const newComponents: Component[] = [];
+    
+        for (const payload of payloads) {
+            const selectedCategory = foodCategoryMap.get(payload.foodCategoryId);
+    
+            if (!selectedCategory) {
+                return new HttpException(`Food category not found for ID ${payload.foodCategoryId}`, 400);
+            }
+
+            const newComponent = new Component();
+            newComponent.name = payload.name;
+            newComponent.component_type = payload.componentType;
+            newComponent.nutrition_info = payload.nutritionInformation;
+            newComponent.units = payload.units;
+            newComponent.amount = payload.amount;
+            newComponent.foodCategory = selectedCategory;
+    
+            // Add storage links
+            newComponent.storage_links = JSON.parse("{}");
+            
+            newComponents.push(newComponent);
+        }
+    
+        try {
+            await this.componentRepository.save(newComponents);
+        } catch (error) {
+            console.error(error);
+            return new HttpException("Error saving components", 400);
+        }
+    
+        return new HttpException("Components added successfully", 200);
+    }
+    
+    async getComponents(decodedHeader: any, type: ComponentType) { 
+        const restricted_food_cat_ids = await this.userAllergyRepository.createQueryBuilder("user_allergy")
+            .select("user_allergy.food_cat_id")
+            .where("user_allergy.user_id = :id", { id: decodedHeader['sub'] })
+            .getMany();
+    
+        const food_cat_ids = restricted_food_cat_ids.map(allergy => allergy.food_cat_id);
+
+        const query = this.componentRepository.createQueryBuilder("component")
+            .select(["component.id", "component.name", "component.storage_links"])
+            .where("component.component_type = :type", { type });
+    
+        if (food_cat_ids.length > 0) {
+            query.andWhere("component.food_cat_id NOT IN (:...ids)", { ids: food_cat_ids });
+        }
+    
+        return await query.getMany();
+    }
+    
+    
+
 }
