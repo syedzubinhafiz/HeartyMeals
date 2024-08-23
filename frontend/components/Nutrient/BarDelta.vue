a3a8b180933c9a5e6fa1215150e0e20ca9a7ca5a
Mario Susanto <msus0004@student.monash.edu>
2024-08-23 19:14:05 +0800

    created nutrient widget variant with value comparisons

---
+++ frontend/components/Nutrient/BarDelta.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="flex flex-col">
+        <div class="flex flex-row justify-between">
+            <div class="flex flex-row">
+                <img :src="icon" alt="icon" class="w-5 h-5 mr-2"/>
+                <P>{{label}}</P>
+            </div>
+            <P :style="{color: color}">{{valueBefore}}/{{maxValue}}mg</P>
+        </div>
+        <div class="bg-blue-200 rounded-md h-2 w-full shadow-md">
+            <div class="rounded-md h-2 shadow-sm" :style="`background-color: ${colorBefore};width: ${Math.max(0, Math.min(100, props.valueBefore / props.maxValue * 100))}%`">
+                <div class="rounded-md h-2 shadow-sm" :style="`background-color: ${colorAfter};width: ${Math.max(0, Math.min(100, props.valueAfter / props.valueBefore * 100))}%`"/>
+            </div>
+        </div>
+    </div>
+    
+</template>
+<script setup>
+defineOptions({
+	name: "NutrientBar",
+});
+
+const props = defineProps({
+    label: {
+		type: String,
+		default: "label",
+	},
+    valueBefore: {
+		type: Number,
+		default: 700,
+	},
+    valueAfter: {
+		type: Number,
+		default: 500,
+	},
+    maxValue: {
+		type: Number,
+		default: 2000,
+	},
+    icon: {
+		type: String,
+		default: "",
+	},
+    colorBefore: {
+		type: String,
+		default: "#555555",
+	},
+    colorAfter: {
+		type: String,
+		default: "#000000",
+	},
+})
+</script>
---
+++ frontend/components/Nutrient/WidgetDelta.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="items-center flex flex-col">
+    <Overlay level="1" class="p-5 space-y-4 z-10 rounded-xl shadow-xl">
+        <H3 class="mx-10">Total Nutrition</H3>
+        <NutrientBarDelta label="Calories" :valueBefore="1800" :valueAfter="1500" :maxValue="2000" colorBefore="#D7D1B4" colorAfter="#B8B396" icon="/assets/img/caloriesIcon.png"/>
+        <NutrientBarDelta label="Carbohydrates" :valueBefore="1500" :valueAfter="1400" :maxValue="2000" colorBefore="#A2D3D6" colorAfter="#83BBBE"  icon="/assets/img/carbIcon.png"/>
+        <NutrientBarDelta label="Protein" :valueBefore="1200" :valueAfter="1000" :maxValue="2000" colorBefore="#99D0A3" colorAfter="#87A98D"  icon="/assets/img/proteinIcon.png"/>
+        <NutrientBarDelta label="Fats" :valueBefore="1000" :valueAfter="800" :maxValue="2000" colorBefore="#FCDEA3" colorAfter="#ECC474"  icon="/assets/img/fatsIcon.png"/>
+        <NutrientBarDelta label="Sodium" :valueBefore="50" :valueAfter="40" :maxValue="100" colorBefore="#F6AA97" colorAfter="#EC7455"  icon="/assets/img/sodiumIcon.png"/>
+        <NutrientBarDelta label="Cholesterols" :valueBefore="800" :valueAfter="400" :maxValue="2000" colorBefore="#F1C9AF" colorAfter="#BE9A83"  icon="/assets/img/cholesterolsIcon.png"/>
+    </Overlay>
+</div>
+    
+</template>
+<script setup>
+
+defineOptions({
+  name: "NutrientWidgetDelta",
+});
+</script>
