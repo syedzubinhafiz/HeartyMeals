5ab8205bef4028a4c95b6b9e628f8cede3c77257
Mario Susanto <msus0004@student.monash.edu>
2024-08-23 21:52:49 +0800

    added background colors to nutrient bars

--- frontend/components/Nutrient/Bar.vue
+++ frontend/components/Nutrient/Bar.vue
@@ -1,13 +1,13 @@
 <template>
     <div class="flex flex-col">
-        <div class="flex flex-row justify-between">
+        <div class="flex flex-row justify-between justify-between space-x-3">
             <div class="flex flex-row">
                 <img :src="icon" alt="icon" class="w-5 h-5 mr-2"/>
                 <P>{{label}}</P>
             </div>
             <P :style="{color: color}">{{value}}/{{maxValue}}mg</P>
         </div>
-        <div class="bg-blue-200 rounded-md h-2 w-full shadow-md">
+        <div class="rounded-md h-2 w-full shadow-md" :style="`background-color: ${bgColor}`">
             <div class="rounded-md h-2 shadow-sm" :style="`background-color: ${color};width: ${Math.max(0, Math.min(100, props.value / props.maxValue * 100))}%`"/>
         </div>
     </div>
@@ -38,6 +38,10 @@ const props = defineProps({
     color: {
 		type: String,
 		default: "#000000",
+	},
+    bgColor: {
+		type: String,
+		default: "#AAAAAA",
 	},
 })
 </script>
--- frontend/components/Nutrient/WidgetDelta.vue
+++ frontend/components/Nutrient/WidgetDelta.vue
@@ -1,13 +1,13 @@
 <template>
     <div class="items-center flex flex-col">
-    <Overlay level="1" class="p-5 space-y-4 z-10 rounded-xl shadow-xl">
+    <Overlay level="1" class="space-y-4 z-10 rounded-xl shadow-xl">
         <H3 class="mx-10">Total Nutrition</H3>
-        <NutrientBarDelta label="Calories" :valueBefore="1800" :valueAfter="1500" :maxValue="2000" colorBefore="#D7D1B4" colorAfter="#B8B396" icon="/assets/img/caloriesIcon.png"/>
-        <NutrientBarDelta label="Carbohydrates" :valueBefore="1500" :valueAfter="1400" :maxValue="2000" colorBefore="#A2D3D6" colorAfter="#83BBBE"  icon="/assets/img/carbIcon.png"/>
-        <NutrientBarDelta label="Protein" :valueBefore="1200" :valueAfter="1000" :maxValue="2000" colorBefore="#99D0A3" colorAfter="#87A98D"  icon="/assets/img/proteinIcon.png"/>
-        <NutrientBarDelta label="Fats" :valueBefore="1000" :valueAfter="800" :maxValue="2000" colorBefore="#FCDEA3" colorAfter="#ECC474"  icon="/assets/img/fatsIcon.png"/>
-        <NutrientBarDelta label="Sodium" :valueBefore="50" :valueAfter="40" :maxValue="100" colorBefore="#F6AA97" colorAfter="#EC7455"  icon="/assets/img/sodiumIcon.png"/>
-        <NutrientBarDelta label="Cholesterols" :valueBefore="800" :valueAfter="400" :maxValue="2000" colorBefore="#F1C9AF" colorAfter="#BE9A83"  icon="/assets/img/cholesterolsIcon.png"/>
+        <NutrientBarDelta label="Calories" :valueBefore="1800" :valueAfter="1500" :maxValue="2000" colorBefore="#D7D1B4" colorAfter="#B8B396" bgColor="#E9E5CD" icon="/assets/img/caloriesIcon.png"/>
+        <NutrientBarDelta label="Carbohydrates" :valueBefore="1500" :valueAfter="1400" :maxValue="2000" colorBefore="#A2D3D6" colorAfter="#83BBBE" bgColor="#E2F3F4"  icon="/assets/img/carbIcon.png"/>
+        <NutrientBarDelta label="Protein" :valueBefore="1200" :valueAfter="1000" :maxValue="2000" colorBefore="#99D0A3" colorAfter="#87A98D" bgColor="#E8F0E9"  icon="/assets/img/proteinIcon.png"/>
+        <NutrientBarDelta label="Fats" :valueBefore="1000" :valueAfter="800" :maxValue="2000" colorBefore="#FCDEA3" colorAfter="#ECC474" bgColor="#FBF1CD"  icon="/assets/img/fatsIcon.png"/>
+        <NutrientBarDelta label="Sodium" :valueBefore="50" :valueAfter="40" :maxValue="100" colorBefore="#F6AA97" colorAfter="#EC7455" bgColor="#F9E1DA"  icon="/assets/img/sodiumIcon.png"/>
+        <NutrientBarDelta label="Cholesterols" :valueBefore="800" :valueAfter="400" :maxValue="2000" colorBefore="#F1C9AF" colorAfter="#BE9A83" bgColor="#FFE5D4"  icon="/assets/img/cholesterolsIcon.png"/>
     </Overlay>
 </div>
     
