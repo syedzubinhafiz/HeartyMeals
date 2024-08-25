ae7f6c5773af1e7ebcf05ea96a0041cfd53acef7
jchu0057 <jchu0057@student.monash.edu>
2024-08-25 14:25:15 +0800

    Added Meal logging repository into the module

--- backend/src/meal-logging/meal-logging.module.ts
+++ backend/src/meal-logging/meal-logging.module.ts
@@ -2,10 +2,16 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { MealLoggingController } from './meal-logging.controller';
 import { MealLoggingService } from './meal-logging.service';
+import { CommonService } from 'src/common/common.service';
+import { User } from 'src/user/user.entity';
+import { Recipe } from 'src/recipe/recipe.entity';
+import { MealLogging } from './meal-logging.entity';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([])],
+    imports: [
+        TypeOrmModule.forFeature([User, Recipe, MealLogging])
+    ],
     controllers: [MealLoggingController],
-    providers: [MealLoggingService]
+    providers: [MealLoggingService, CommonService]
 })
 export class MealLoggingModule {}
