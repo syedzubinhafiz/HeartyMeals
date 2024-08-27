78eaec9fd3df72904e9cb1b05e0abaace6d7d3bd
Mario Susanto <msus0004@student.monash.edu>
2024-08-27 17:42:29 +0800

    added structure for stomach meal cards

---
+++ frontend/components/Stomach/MealCard.vue
@@ -0,0 +1,24 @@
+<template>
+    <div level="2" class="flex flex-row items-center p-2 space-x-4">
+        <!-- <div class="w-16 h-16"/> -->
+         <img src="../../public/assets/img/croissant.svg" class="h-24 w-24 rounded-lg shadow-md"/>
+         <div class="flex flex-col grow">
+            <H3>meal name</H3>
+            <P>Serving Size:</P>
+            <p class="text-xs">Calories: Protein:</p>
+            <p class="text-xs">Fat: Fibre: Sodium:</p>
+            <p class="text-xs">Cholesterol:</p>
+         </div>
+         <div class="flex flex-row ml-5 space-x-2">
+            <P>servings:</P>
+            <Dropdown :options="[0.25,0.5,0.75,1,2,3,4]" v-model="servings"/>
+         </div>
+    </div>
+</template>
+<script setup>
+defineOptions({
+  name: 'StomachMeal',
+});
+
+const servings = ref(1)
+</script>
--- frontend/components/Stomach/Sidebar.vue
+++ frontend/components/Stomach/Sidebar.vue
@@ -1,7 +1,7 @@
 <template>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
   <div v-if="computedSidebarOpen" class="fixed inset-0 bg-black bg-opacity-50 z-50">
-    <div class="fixed top-0 right-0 w-96 bg-custom-overlay-brown h-full shadow-md z-50 p-5 pb-16 rounded-l-lg">
+    <div class="fixed top-0 right-0 bg-custom-overlay-brown h-full shadow-md z-50 p-5 pb-16 rounded-l-lg">
       <div class="flex justify-end w-full">
         <button @click="toggleSidebar" class="text-black">
           <i class="bi bi-x text-3xl"></i>
@@ -11,9 +11,9 @@
         <H2>Stomach</H2>
         <hr>
         <div class="grow flex flex-col space-y-2 overflow-y-auto w-full">
-          <P>meal 1</P>
-          <P>meal 2</P>
-          <P>meal 3</P>
+          <StomachMealCard/>
+          <StomachMealCard/>
+          <StomachMealCard/>
         </div>
         <ButtonGreen>Summary</ButtonGreen>
       </div>
