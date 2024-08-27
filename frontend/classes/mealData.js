50303fb40b8edf380d475ec64acd50e0e2f6995b
Mario Susanto <msus0004@student.monash.edu>
2024-08-27 18:13:05 +0800

    added stomach meal card data

---
+++ frontend/classes/mealData.js
@@ -0,0 +1,10 @@
+export default class MealData {
+    constructor(name,mealImg,servingSize,servings,nutrientData) {
+        this.name = name
+        this.mealImg = mealImg
+        this.servingSize = servingSize
+        this.servings = servings
+        this.nutrientData = nutrientData
+    }
+
+}
