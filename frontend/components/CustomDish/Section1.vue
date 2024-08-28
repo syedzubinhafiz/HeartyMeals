3d9a0844543c9fb47076bc74b00420eebb41d761
Mario Susanto <msus0004@student.monash.edu>
2024-08-28 19:32:35 +0800

    added functionality for searching in custom meal creation

--- frontend/components/CustomDish/Section1.vue
+++ frontend/components/CustomDish/Section1.vue
@@ -1,9 +1,9 @@
 <template>
 	<div class="flex w-full h-full grow p-2 space-x-4">
 		<div class="w-2/3 flex flex-col space-y-2">
-			<SearchBar/>
-			<H2>Recent Searches</H2>
-			<CustomDishIngredientList :columns="2" :ingredientList="ingredientList" buttonStr="+" :buttonOnClick="onAdd" :includeDropdown="true" class="overflow-y-auto"  style="max-height:50vh"/>
+			<SearchBar v-model="searchVal" :dataList="ingredientNameList"/>
+			<H2>{{ searchVal.length>0 ? "Search" : "Recent Searches" }}</H2>
+			<CustomDishIngredientList :columns="2" :ingredientList="filteredIngredientList" buttonStr="+" :buttonOnClick="onAdd" :includeDropdown="true" class="overflow-y-auto"  style="max-height:50vh"/>
 		</div>
 		<div class="w-1/3 flex flex-col space-y-2">
 			<H2>Selected Ingredients</H2>
@@ -36,4 +36,37 @@ const onAdd = (ingredient) => {
 const onRemove = (ingredient) => {
 	props.modelValue.ingredientList = props.modelValue.ingredientList.filter(item => item !== ingredient)
 }
+
+const searchVal = ref("")
+
+
+const ingredientNameList = computed({
+	get() {
+		return props.ingredientList.map((ingredient) => {return ingredient.name})
+	}
+})
+
+const filteredIngredientList = computed({
+	get() {
+		return props.ingredientList.filter((item) => {
+			let search = searchVal.value.toLowerCase()
+			let name = item.name.toLowerCase()
+			let splitNames = name.split(" ")
+			let i = splitNames.length - 1
+			let currentName = splitNames[i].toLowerCase()
+			if(search.length <= currentName.length && search === currentName.slice(0, search.length)) {
+				return true
+			}
+			i -= 1
+			while(i>=0) {
+				currentName = splitNames[i].toLowerCase() + " " + currentName
+				if(search.length <= currentName.length && search === currentName.slice(0, search.length)) {
+					return true
+				}
+				i -= 1
+			}
+			return false
+		});
+	}
+})
 </script>
--- frontend/components/CustomDish/Section2.vue
+++ frontend/components/CustomDish/Section2.vue
@@ -1,9 +1,9 @@
 <template>
 	<div class="flex w-full h-full grow p-2 space-x-4">
 		<div class="w-2/3 flex flex-col space-y-2">
-			<SearchBar/>
-			<H2>Recent Searches</H2>
-			<CustomDishIngredientList :columns="2" :ingredientList="seasoningList" buttonStr="+" :buttonOnClick="onAdd" :includeDropdown="true" class="overflow-y-auto"  style="max-height:50vh"/>
+			<SearchBar v-model="searchVal" :dataList="seasoningNameList"/>
+			<H2>{{ searchVal.length>0 ? "Search" : "Recent Searches" }}</H2>
+			<CustomDishIngredientList :columns="2" :ingredientList="filteredSeasoningList" buttonStr="+" :buttonOnClick="onAdd" :includeDropdown="true" class="overflow-y-auto"  style="max-height:50vh"/>
 		</div>
 		<div class="w-1/3 flex flex-col space-y-2">
 			<H2>Selected Seasoning</H2>
@@ -36,4 +36,37 @@ const onAdd = (ingredient) => {
 const onRemove = (ingredient) => {
 	props.modelValue.seasoningList = props.modelValue.seasoningList.filter(item => item !== ingredient)
 }
+
+const searchVal = ref("")
+
+
+const seasoningNameList = computed({
+	get() {
+		return props.seasoningList.map((ingredient) => {return ingredient.name})
+	}
+})
+
+const filteredSeasoningList = computed({
+	get() {
+		return props.seasoningList.filter((item) => {
+			let search = searchVal.value.toLowerCase()
+			let name = item.name.toLowerCase()
+			let splitNames = name.split(" ")
+			let i = splitNames.length - 1
+			let currentName = splitNames[i].toLowerCase()
+			if(search.length <= currentName.length && search === currentName.slice(0, search.length)) {
+				return true
+			}
+			i -= 1
+			while(i>=0) {
+				currentName = splitNames[i].toLowerCase() + " " + currentName
+				if(search.length <= currentName.length && search === currentName.slice(0, search.length)) {
+					return true
+				}
+				i -= 1
+			}
+			return false
+		});
+	}
+})
 </script>
