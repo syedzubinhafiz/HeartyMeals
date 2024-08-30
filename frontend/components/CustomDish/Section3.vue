c782e4c1a8c8440e3c36f6474eeb5da9c9f0a223
Mario Susanto <msus0004@student.monash.edu>
2024-08-30 17:07:31 +0800

    added inputs for additional info in custom meal popup

--- frontend/components/CustomDish/Section3.vue
+++ frontend/components/CustomDish/Section3.vue
@@ -1,12 +1,58 @@
 <template>
-	<div class="flex flex-col w-full h-full grow p-2">
-		<div class="flex justify-center items-center p-2">
-			<div class="border border-black rounded-lg">
-				<ImgDragbox v-model="modelValue.imgSrc"/>
+	<div class="flex-col w-full h-full grow">
+		<div class="w-full flex p-2 space-x-4">
+			<div class="w-1/2 flex flex-col space-y-2">
+				<div class="flex justify-center items-center p-2">
+					<div class="border border-black rounded-lg">
+						<ImgDragbox v-model="modelValue.imgSrc"/>
+					</div>
+				</div>
+			</div>
+			<div class="w-1/2 flex flex-col space-y-2">
+				<H3>Recommended Meal Time</H3>
+				<div class="flex space-x-2 items-center justify-between">
+					<div class="space-x-2">
+						<label for="cbBreakfast">Breakfast</label>
+						<input v-model="modelValue.breakfast" type="checkbox" id="cbBreakfast" class="translate-y-[2px]"/>
+					</div>
+					<div class="space-x-2">
+						<label for="cbLunch">Lunch</label>
+						<input v-model="modelValue.lunch" type="checkbox" id="cbLunch" class="translate-y-[2px]"/>
+					</div>
+					<div class="space-x-2">
+						<label for="cbDinner">Dinner</label>
+						<input v-model="modelValue.dinner"  type="checkbox" id="cbDinner" class="translate-y-[2px]"/>
+					</div>
+					<div class="space-x-2">
+						<label for="cbSnack">Snack</label>
+						<input v-model="modelValue.snack" type="checkbox" id="cbSnack" class="translate-y-[2px]"/>
+					</div>
+
+				</div>
+				<div class="flex">
+					<div class="w-1/2">
+						<H3>Recipe Visibility</H3>
+						<Dropdown v-model="modelValue.visibility" :options="['unlisted','private','public']"/>
+						<H3>Preparation Time</H3>
+						<div class="flex space-x-2">
+							<Input v-model="modelValue.prepTime" type="number" placeholder="" class="w-2/5"/>
+							<p>minutes</p>
+						</div>
+						
+						<H3>Recipe Serving</H3>
+						<Input v-model="modelValue.recipeServing" type="number" placeholder="" class="w-2/5"/>
+					</div>
+					<div class="w-1/2">
+						<H3>Diet</H3>
+						<Dropdown v-model="modelValue.dietaryID"/>
+						<H3>Cuisine</H3>
+						<Dropdown v-model="modelValue.cuisineID"/>
+					</div>
+				</div>
+
 			</div>
 		</div>
-		
-		<div class="flex h-full">
+		<div class="w-full flex p-2 space-x-4">
 			<div class="w-1/2 p-2 space-y-2">
 				<H3>Recipe Corner</H3>
 				<CustomDishIngredientList :columns="1" :ingredientList="modelValue.ingredientList" :includeInput="true" class="overflow-y-auto"  style="max-height:30vh"/>
