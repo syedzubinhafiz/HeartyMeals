2563174dab0c5841403f5ac5386bad807144c0ba
jchu0057 <jchu0057@student.monash.edu>
2024-09-01 16:22:36 +0800

    Reupdated the code to cater for null case

--- backend/src/allergy/user_allergy.service.ts
+++ backend/src/allergy/user_allergy.service.ts
@@ -53,19 +53,13 @@ export class UserAllergyService{
                 .andWhere('food_cat_id = :food_cat_id', {food_cat_id: id})
                 .getOne();
                 // only add the ones that do not exist in the database
-                if (!user_allergy){
-                    if (user_allergy.deleted_at != null){
-                        // if the entry is soft deleted, reactivate the entry
-                        user_allergy.deleted_at = null;
-                        all_entries.push(user_allergy);
-                    } else {
-                        // Create entries to store in saved_entries
-                        const new_user_allergy = new UserAllergy();
-                        new_user_allergy.user = user_object;
-                        new_user_allergy.foodCategory = food_category;
-                        
-                        all_entries.push(new_user_allergy);
-                    }
+                if (!user_allergy || user_allergy == null){
+                    // Create entries to store in saved_entries
+                    const new_user_allergy = new UserAllergy();
+                    new_user_allergy.user = user_object;
+                    new_user_allergy.foodCategory = food_category;
+                    
+                    all_entries.push(new_user_allergy)
                 }
 
             }));
