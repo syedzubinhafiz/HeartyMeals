495ae77e59899282a02f50fcd17f2c347f9c2e36
jchu0057 <jchu0057@student.monash.edu>
2024-10-03 00:06:18 +0800

    fixed meal logging not getting recipe images issue

--- backend/src/meal-logging/meal-logging.module.ts
+++ backend/src/meal-logging/meal-logging.module.ts
@@ -13,12 +13,14 @@ import { Country } from 'src/country/country.entity';
 import { Dietary } from 'src/dietary/dietary.entity';
 import { Ethnicity } from 'src/ethnicity/ethnicity.entity';
 import { MealLogSummaryService } from 'src/meal-log-summary/meal-log-summary.service';
+import { StorageService } from 'src/storage/storage.service';
+import { Storage } from 'src/storage/storage.entity';
 
 @Module({
     imports: [
-        TypeOrmModule.forFeature([MealLogSummary, User, MealLogging, Recipe, Country, Dietary, Ethnicity, ])
+        TypeOrmModule.forFeature([MealLogSummary, User, MealLogging, Recipe, Country, Dietary, Ethnicity, Storage])
     ],
     controllers: [MealLoggingController, MealPlanningController],
-    providers: [MealLoggingService, CommonService, UserService, MealLogSummaryService]
+    providers: [MealLoggingService, CommonService, UserService, MealLogSummaryService, StorageService]
 })
 export class MealLoggingModule {}
