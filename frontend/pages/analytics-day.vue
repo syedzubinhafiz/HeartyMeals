556516c8ceef2b06fcd928f7c28f6bdfaa090246
Mario Susanto <msus0004@student.monash.edu>
2024-10-05 17:20:51 +0800

    fix

--- frontend/pages/analytics-day.vue
+++ frontend/pages/analytics-day.vue
@@ -149,14 +149,14 @@ const getData = async () => {
   console.log(currentDate)
   analyticsData.value = await useApi(`/analytics/daily?date=${currentDate}&timeZone=Asia/Kuala_Lumpur`,"GET")
   console.log(analyticsData.value)
-  breakfastTotal.value = analyticsData.value.value.breakfast_total
-  breakfastList.value = analyticsData.value.value.breakfast
-  lunchTotal.value = analyticsData.value.value.lunch_total
-  lunchList.value = analyticsData.value.value.lunch
-  dinnerTotal.value = analyticsData.value.value.dinner_total
-  dinnerList.value = analyticsData.value.value.dinner
-  otherTotal.value = analyticsData.value.value.other_total
-  otherList.value = analyticsData.value.value.other
+  breakfastTotal.value = analyticsData.value.value.breakfast_total ?? analyticsData.value.value.Breakfast_total 
+  breakfastList.value = analyticsData.value.value.breakfast ?? analyticsData.value.value.Breakfast 
+  lunchTotal.value = analyticsData.value.value.lunch_total ?? analyticsData.value.value.Lunch_total 
+  lunchList.value = analyticsData.value.value.lunch ?? analyticsData.value.value.Lunch 
+  dinnerTotal.value = analyticsData.value.value.dinner_total ?? analyticsData.value.value.Dinner_total 
+  dinnerList.value = analyticsData.value.value.dinner ?? analyticsData.value.value.Dinner 
+  otherTotal.value = analyticsData.value.value.other_total ?? analyticsData.value.value.Other_total 
+  otherList.value = analyticsData.value.value.other ?? analyticsData.value.value.Other 
 
   console.log(analyticsData.value.value)
   let result = await useApi(`/user/budget?startDate=${currentDate}&timeZone=Asia/Kuala_Lumpur`, "GET");
