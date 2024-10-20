0755032ef3d8d5c9701af0e9ee2dc05ad68f941d
Mario Susanto <msus0004@student.monash.edu>
2024-10-20 13:59:45 +0800

    added base folder module for jest into package.json and added mocks for analytics

--- backend/src/analytics/analytics.service.spec.ts
+++ backend/src/analytics/analytics.service.spec.ts
@@ -1,12 +1,54 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AnalyticsService } from './analytics.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { MealLogSummary } from 'src/meal-log-summary/meal-log-summary.entity';
+import { MealLogging } from 'src/meal-logging/meal-logging.entity';
+import { User } from 'src/user/user.entity';
 
 describe('AnalyticsService', () => {
   let service: AnalyticsService;
 
+  // Mock Repositories
+  const mockMealLogSummaryRepository = {
+    find: jest.fn(),
+    createQueryBuilder: jest.fn().mockReturnValue({
+      where: jest.fn().mockReturnThis(),
+      andWhere: jest.fn().mockReturnThis(),
+      getOne: jest.fn(),
+      getMany: jest.fn(),
+    }),
+  };
+
+  const mockMealLoggingRepository = {
+    find: jest.fn(),
+    createQueryBuilder: jest.fn().mockReturnValue({
+      leftJoinAndSelect: jest.fn().mockReturnThis(),
+      where: jest.fn().mockReturnThis(),
+      getMany: jest.fn(),
+    }),
+  };
+
+  const mockUserRepository = {
+    findOneByOrFail: jest.fn(),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AnalyticsService],
+      providers: [
+        AnalyticsService,
+        {
+          provide: getRepositoryToken(MealLogSummary),
+          useValue: mockMealLogSummaryRepository,
+        },
+        {
+          provide: getRepositoryToken(MealLogging),
+          useValue: mockMealLoggingRepository,
+        },
+        {
+          provide: getRepositoryToken(User),
+          useValue: mockUserRepository,
+        },
+      ],
     }).compile();
 
     service = module.get<AnalyticsService>(AnalyticsService);
@@ -15,4 +57,7 @@ describe('AnalyticsService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  // Add additional tests for each method of the AnalyticsService
 });
+
