0960067639ce54757c03228ca378071c02f8a3f3
jchu0057 <jchu0057@student.monash.edu>
2024-10-23 21:36:11 +0800

    removed unsused stuff from module

--- backend/src/storage/storage.module.ts
+++ backend/src/storage/storage.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Storage } from './storage.entity';
-import { StorageController } from './storage.controller';
 import { StorageService } from './storage.service';
 import { User } from 'src/user/user.entity';
 import { Country } from 'src/country/country.entity';
@@ -11,7 +10,7 @@ import { Ethnicity } from 'src/ethnicity/ethnicity.entity';
 
 @Module({
     imports: [TypeOrmModule.forFeature([Storage, User, Country, Dietary, Ethnicity])],
-    controllers: [StorageController],
+    controllers: [],
     providers: [StorageService]
 })
 export class StorageModule {}
